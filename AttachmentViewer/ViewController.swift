3fd632fa05e9286b6907e4ec6daa8932069f972f
shima <shima.jin@icloud.com>
2017-06-04 13:51:41 +0900

    Initial Commit

---
+++ AttachmentViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AttachmentViewer
+//
+//  Created by jinsei shima on 2017/06/04.
+//  Copyright © 2017年 jinsei shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
